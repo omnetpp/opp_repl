704eca88e1f23d3fed240e9376cd0704c56a1e8f
Levente Meszaros <levy@omnetpp.org>
2026-04-24 16:22:54 +0200

    Fixed profile relative path.

--- opp_repl/test/profile.py
+++ opp_repl/test/profile.py
@@ -14,7 +14,7 @@ def generate_profile_report(simulation_project=None, output_file="perf.data", **
         simulation_project = get_default_simulation_project()
     simulation_task_results = run_simulations(simulation_project=simulation_project, mode="profile", prepend_args=["perf", "record", "-g", "--call-graph", "dwarf", "-o", output_file], **kwargs)
     working_directory = simulation_task_results.results[0].task.simulation_config.working_directory
-    return simulation_project.get_relative_path(os.path.join(working_directory, output_file))
+    return os.path.join(working_directory, output_file)
 
 def open_profile_report(simulation_project=None, output_file="perf.data", **kwargs):
     if simulation_project is None:
